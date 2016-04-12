f074f52475d4ba07b0bf83bc218cb50ceb0d47b3
Yana Petrova <yana.logical@gmail.com>
2016-04-12 15:55:17 +0600

    День 1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program1_1
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.1
+            //int a;
+            //int b;
+            //int c;
+
+            //a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+
+            //c = a + b;
+
+            //Console.WriteLine(c);
+            //Console.ReadKey();
+
+
+            //Задание 1.2
+            //int n;
+            //int i;
+            //int value;
+
+            //n = Convert.ToInt32(Console.ReadLine());
+            //i = Convert.ToInt32(Console.ReadLine());
+
+            //value = (n >> (i - 1)) & 1;
+
+            //Console.WriteLine(value);
+            //Console.ReadKey();
+
+            //Задание 1.3
+            int n;
+            int value;
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            value = n >> 1;
+            value = value << 1;
+
+            Console.WriteLine(value);
+            Console.ReadKey();
+
+
+        }
+    }
+}
