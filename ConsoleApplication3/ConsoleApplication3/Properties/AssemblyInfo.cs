eec85ed7813d66b7b92788f4c65e3f9047af9153
Yana Petrova <yana.logical@gmail.com>
2016-04-14 22:23:58 +0600

    Правки

--- ConsoleApplication3/ConsoleApplication3/Properties/AssemblyInfo.cs
+++ ConsoleApplication3/ConsoleApplication3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication3")]
+[assembly: AssemblyTitle("Exercise2.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication3")]
+[assembly: AssemblyProduct("Exercise2.1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
