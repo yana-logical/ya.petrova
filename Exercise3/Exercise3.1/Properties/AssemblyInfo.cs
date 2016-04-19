38214ade377f6ee91ffb43750cff4b24799ca59b
Yana Petrova <yana.logical@gmail.com>
2016-04-19 17:18:03 +0600

    Изменение структуры папок

--- Exercise3/Exercise3.1/Properties/AssemblyInfo.cs
+++ Exercise3/Exercise3.1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication2")]
+[assembly: AssemblyTitle("ConsoleApplication6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication2")]
+[assembly: AssemblyProduct("ConsoleApplication6")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("259d2189-e1b0-418d-866d-c8b6b96475de")]
+[assembly: Guid("906816e0-2e65-43d8-9277-1606fc54d78b")]
 
 // Version information for an assembly consists of the following four values:
 //
