2b5db484d775a4c651dbf4fc561a0b45fa34f32d
Yana Petrova <yana.logical@gmail.com>
2016-04-26 16:15:22 +0600

    Задания с лекции 4

---
+++ Exercise4/Exercise4.2/Phone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4._2
+{
+    public class Phone
+    {
+        private readonly string _cityCode;
+        private readonly string _phoneNumber;
+
+        public Phone(string cityCode, string phoneNumber)
+        {
+            _cityCode = cityCode;
+            _phoneNumber = phoneNumber;
+        }
+
+        public string GetPhone
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_cityCode))
+                {
+                    return (_phoneNumber);
+                }
+                else
+                {
+                    return ("(" + _cityCode + ") " + _phoneNumber);
+                }
+            } 
+        }
+    }
+}
---
+++ Exercise4/Exercise4.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Преобразовать класс “Телефон” на работу со свойствами
+
+            Console.WriteLine("Введите код города и телефона через Enter");
+
+            string cityCode = Console.ReadLine();
+            string phoneNuber = Console.ReadLine();
+
+            Phone phone1 = new Phone(cityCode, phoneNuber);
+
+            Console.WriteLine();
+            Console.WriteLine(phone1.GetPhone);
+            Console.ReadKey();
+        }
+    }
+}
