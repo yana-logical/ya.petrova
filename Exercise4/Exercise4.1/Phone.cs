38f1de5db3ad16150b8e0352d15321e7f72a4a41
Yana Petrova <yana.logical@gmail.com>
2016-04-26 16:16:15 +0600

    Задания с лекции 4

---
+++ Exercise4/Exercise4.1/Phone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    public class Phone
+    {
+        private readonly string _cityCode;
+        private readonly string _phoneNumber;
+
+        public Phone(string cityCode, string phoneNumber)
+        {
+            _cityCode = cityCode;
+            _phoneNumber = phoneNumber;
+        }
+
+        public string GetPhone()
+        {
+            if (string.IsNullOrEmpty(_cityCode))
+            {
+                return (_phoneNumber);
+            }
+            else
+            {
+                return ("("+_cityCode+") "+_phoneNumber);
+            }
+        } 
+    }
+}
---
+++ Exercise4/Exercise4.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создать класс “Телефон”, который содержит следующие данные:
+            //код города, номер телефона, которые не могут быть изменены. 
+            //Добавить метод, который возвращает строку вида “(999) 999999” или “999999”, если код города отсутствует.
+
+            Console.WriteLine("Введите код города и телефона через Enter");
+
+            string cityCode = Console.ReadLine();
+            string phoneNuber = Console.ReadLine();
+
+            Phone phone1 = new Phone(cityCode, phoneNuber);
+
+            Console.WriteLine();
+            Console.WriteLine(phone1.GetPhone());
+            Console.ReadKey();
+
+        }
+    }
+}
